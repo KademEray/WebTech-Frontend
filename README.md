158970d8944696a5075f8b8d49d94497be5cd2e7
eray5 <eray55kadem@gmail.com>
2023-10-21 16:48:57 +0200

    bearbeitung

---
+++ README.md
@@ -0,0 +1,28 @@
+# frontend
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+### run dev
+```
+npm run dev
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HighscoresList />
+  </div>
+</template>
+
+<script>
+import HighscoresList from './components/HighscoresList.vue'
+
+export default {
+  components: {
+    HighscoresList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
